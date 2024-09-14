22fbe800139317e2b8c12b331b1972c48a6dac54
Rahul <rahulguptaji1019@gmail.com>
2024-09-14 18:18:15 +0530

    Updated routes

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import  {AllProject, createProject, DeleteProjectbyID, ProjectbyID, UpdateProjectbyID } from '../controllers/project.controller'
 import { AddMember, RemoveMember } from '../controllers/members.controller'
 import { Authenticate } from '../middlewares/Auth'
+import { OwnershipCheck } from '../middlewares/Authorize'
 
 export const projects = express.Router()
 
@@ -13,12 +14,12 @@ projects.get('/', AllProject )
 
 projects.get('/:projectID', Authenticate, ProjectbyID )
 
-projects.put('/:projectID', Authenticate, UpdateProjectbyID )
+projects.put('/:projectID', Authenticate, OwnershipCheck , UpdateProjectbyID )
 
-projects.delete('/:projectID', Authenticate, DeleteProjectbyID )
+projects.delete('/:projectID', Authenticate, OwnershipCheck, DeleteProjectbyID )
 
 // Project Members
 
-projects.post('/:projectID/members', AddMember )
+projects.post('/:projectID/members', Authenticate, AddMember )
 
-projects.delete('/:projectID/members/:userId', RemoveMember)
+projects.delete('/:projectID/members/:userId', Authenticate, RemoveMember)
