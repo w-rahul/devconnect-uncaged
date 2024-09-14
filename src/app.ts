ce8762ef88a5ec3ce6487336119c067b0a329995
Rahul <rahulguptaji1019@gmail.com>
2024-09-14 16:11:25 +0530

    Added dotenv config

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import { auth } from './routes/auth'
 import { projects } from './routes/projects'
 import { code } from './routes/code'
 import { chat } from './routes/chat'
+import { PrismaClient } from '@prisma/client'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 const PORT = 8080
+export const prisma = new PrismaClient()
 
 app.use(express.json())
 
