b96c84b3b59348e09f45144e4d08a73e17e640e7
Rahul <rahulguptaji1019@gmail.com>
2024-09-15 02:47:48 +0530

    Added the Cloudinary

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,17 @@ import { code } from './routes/code'
 import { chat } from './routes/chat'
 import { PrismaClient } from '@prisma/client'
 import dotenv from "dotenv"
+import cloudinary from 'cloudinary';
 
 dotenv.config()
+
+dotenv.config();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express()
 const PORT = 8080
