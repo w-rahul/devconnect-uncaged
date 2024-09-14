35a442aa34b1c08ff1dd0d183175ce8a0c668069
Rahul <rahulguptaji1019@gmail.com>
2024-09-14 17:31:53 +0530

    Added Middleware in Project routes

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,20 +1,21 @@
 import express from 'express'
 import  {AllProject, createProject, DeleteProjectbyID, ProjectbyID, UpdateProjectbyID } from '../controllers/project.controller'
 import { AddMember, RemoveMember } from '../controllers/members.controller'
+import { Authenticate } from '../middlewares/Auth'
 
 export const projects = express.Router()
 
 //Projects
 
-projects.post('/', createProject )
+projects.post('/', Authenticate , createProject )
 
 projects.get('/', AllProject )
 
-projects.get('/:projectID', ProjectbyID )
+projects.get('/:projectID', Authenticate, ProjectbyID )
 
-projects.put('/:projectID', UpdateProjectbyID )
+projects.put('/:projectID', Authenticate, UpdateProjectbyID )
 
-projects.delete('/:projectID', DeleteProjectbyID )
+projects.delete('/:projectID', Authenticate, DeleteProjectbyID )
 
 // Project Members
 
