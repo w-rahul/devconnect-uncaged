e54592d5c47118389b93a9520d34eec0d28ed52e
Rahul <rahulguptaji1019@gmail.com>
2024-09-15 02:47:12 +0530

    Upadted Code route

--- src/routes/code.ts
+++ src/routes/code.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 import { DeleteFilebyID, getFile, getFilebyID, PostFile, UpdateFilebyID } from '../controllers/code.controller'
+import multer from "multer"
+import { Authenticate } from '../middlewares/Auth'
+import { CheckMember } from '../middlewares/CheckMember'
+import { CheckCreatorOrContributor } from '../middlewares/CreatorOrContributor'
+
+
+const storage = multer.memoryStorage()
+const upload = multer ({ storage })
 
 export const code = express.Router()
 
-code.post('/:id/code', PostFile )
+code.post('/:ProjectId/code/:UserId/', Authenticate, CheckMember, CheckCreatorOrContributor, upload.single('file'), PostFile);
 
-code.get('/code', getFile)
+code.get('/code', Authenticate, getFile);
 
-code.get('/:id/code', getFilebyID)
+code.get('/:ProjectId/code', Authenticate, getFilebyID);
 
-code.put('/:id/code/:submissionId', UpdateFilebyID )
+code.put('/:ProjectId/code/:submissionId', Authenticate, CheckMember, CheckCreatorOrContributor, upload.single('file'), UpdateFilebyID);
 
-code.delete('/:id/code/:submissionId', DeleteFilebyID )
+code.delete('/:ProjectId/code/:submissionId', Authenticate, CheckMember, CheckCreatorOrContributor, DeleteFilebyID);
