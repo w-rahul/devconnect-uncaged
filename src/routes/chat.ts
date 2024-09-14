0aa56d4e943aba6a8da227292c5484e4154131aa
Rahul <rahulguptaji1019@gmail.com>
2024-09-14 23:46:53 +0530

    Added ProjectId in chat route

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { CreateChat, GetChat } from "../controllers/chat.controller";
+import { Authenticate } from "../middlewares/Auth";
+import { CheckMember } from "../middlewares/CheckMember";
 
 export const chat  = express.Router()
 
-chat.post('/:id', CreateChat )
+chat.post('/:projectID/:userID', Authenticate, CheckMember, CreateChat);
 
-chat.get('/:id', GetChat )
+chat.get('/:projectID/:userID', Authenticate, CheckMember, GetChat);
