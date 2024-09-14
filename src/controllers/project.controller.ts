c17ae50bb17c64705daad6cd5bc321f7322194d8
Rahul <rahulguptaji1019@gmail.com>
2024-09-14 17:31:14 +0530

    Added Project Controllers Logic

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,21 +1,158 @@
 import { Response, Request } from "express";
+import z, { string } from 'zod'
+import { prisma } from "../app";
 
-export const createProject = (req: Request, res: Response)=>{
+const createProjectSchema = z.object({
+    name : z.string().min(1),
+    description: z.string(),
+    techStack : z.array(z.string()),
+    creatorID : z.string()
+})
+
+export const createProject = async (req: Request, res: Response)=>{
+    const body = req.body
+    const success = createProjectSchema.safeParse(body)
+    const UserID = req.userId
+
+    if(!success.success){
+        return res.status(400).json({message : "Invalid inputs"})
+    }
+
+    const ProjectExists = await prisma.project.findUnique({
+        where:{
+            name : body.name
+        }
+    })
+
+    if(ProjectExists){
+        return res.status(400).json({message : "Project name already taken"})
+    }
+
+    const NewProject = await prisma.project.create({
+        data:{
+            name : body.name,
+            description : body.description,
+            techStack : body.techStack,
+            creatorID : UserID || "",
+        }
+    })
+
+    return res.status(200).json({
+        messge : "Project created",
+        NewProject
+    })
 
 }
 
-export  const AllProject = (req: Request, res: Response)=>{
+export  const AllProject = async  (req: Request, res: Response)=>{
+ 
+    try {
+        const projects = await prisma.project.findMany();
+
+        return res.status(200).json({
+             projects
+         })
+    } 
+    catch (error) {
+        return res.status(500).json({ message: "Error fetching projects" });
+    }
+}
+
+export  const ProjectbyID = async (req: Request, res: Response)=>{
+
+    const { id } = req.params
+    if (!id) return res.status(400).json({
+         message: "Project ID is required"
+     })
+
+    try {
+        const project = await prisma.project.findUnique({
+             where: {
+                 id
+             }
+         })
+        if (!project) return res.status(404).json({
+             message: "Project not found" 
+        })
+
+        return res.status(200).json({
+             project
+        })
+    } 
+    catch (error) {
+        return res.status(500).json({ message: "Error fetching project" });
+    }
 
 }
 
-export  const ProjectbyID = (req: Request, res: Response)=>{
 
+const updateProjectSchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    techStack: z.array(z.string()).optional(),
+})
+
+
+export  const UpdateProjectbyID = async (req: Request, res: Response)=>{
+
+    const { id } = req.params;
+    const body = req.body;
+
+    const result = updateProjectSchema.safeParse(body);
+
+    if (!result.success) {
+        return res.status(400).json({ message: "Invalid input", errors: result.error.errors });
+    }
+
+    try {
+
+        const projectExists = await prisma.project.findUnique({
+             where: {
+                 id
+            }
+         })
+        if (!projectExists) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+
+        const updatedProject = await prisma.project.update({
+            where: { id },
+            data: body,
+        })
+
+        return res.status(200).json({ project: updatedProject })
+
+    } catch (error) {
+
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-export  const UpdateProjectbyID = (req: Request, res: Response)=>{
+export  const DeleteProjectbyID =async (req: Request, res: Response)=>{
 
-}
+    const { id } = req.params;
 
-export  const DeleteProjectbyID = (req: Request, res: Response)=>{
+    try {
 
+        const projectExists = await prisma.project.findUnique({
+             where: {
+                 id 
+            } 
+        })
+        if (!projectExists) {
+            return res.status(404).json({ message: "Project not found" });
+        }
+
+        await prisma.project.delete({
+             where: {
+                 id
+            }
+        })
+
+        return res.status(200).json({ message: "Project deleted successfully" });
+
+    } catch (error) {
+
+        return res.status(500).json({ message: "Internal server error" });
+    }
 }
