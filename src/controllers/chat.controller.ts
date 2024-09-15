e231a178f42831985e99fa212ee4d5465d54cd2b
Rahul <rahulguptaji1019@gmail.com>
2024-09-15 14:30:28 +0530

    Added Chat controllers

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,9 +1,56 @@
 import { Request, Response } from "express";
+import { prisma } from "../app";
 
-export const CreateChat = () =>{
-    
+export const CreateChat = async (req: Request, res: Response) =>{
+    const {projectID, userID} = req.params
+    const message  = req.body.message
+
+    try {
+
+        const chat = await prisma.chat.create({
+            data:{
+                projectID : projectID,
+                userID : userID,
+                message : message
+            }
+        })
+
+        req.app.get('socketio').to(projectID).emit('receiveMessage', {message, userID})
+        
+        return res.status(200).json(chat)
+
+    } catch (error) {
+       
+        console.log(error)
+        return res.status(500).json({
+            message : "Error creating chat message"
+        })
+    }
+
 }
 
-export const GetChat = () =>{
+export const GetChat = async (req: Request, res: Response) =>{
 
+    const { projectID} = req.params
+    
+    try {
+        
+        const AllChats = await prisma.chat.findMany({
+            where:{
+                projectID : projectID
+            },
+            orderBy:{
+                createdAt : 'asc'
+            }
+        })
+
+        return res.status(200).json(AllChats)
+
+    } catch (error) {
+        console.log(error)
+
+        return res.status(500).json({
+            message : "Error getting chat"
+        })
+    }
 }
