170fa780bedf4e8c8e6363a2e2272b3127437353
Rahul <rahulguptaji1019@gmail.com>
2024-09-16 07:52:50 +0530

    Added return after next() and fixed error handling

--- src/middlewares/Authorize.ts
+++ src/middlewares/Authorize.ts
@@ -3,10 +3,14 @@ import { prisma } from "../app"
 
 export const OwnershipCheck = async (req: Request, res: Response, next : NextFunction) => {
     const userId = req.userId
-    const projectId = req.params.id
+    const projectId = req.params.projectID
 
     try{
-        const project = await prisma.project.findUnique({ where: {id : projectId }})
+        const project = await prisma.project.findUnique({ 
+            where: {
+                id : projectId 
+            }
+        })
 
         if(!project){
             return res.status(404).json({message : "Project not found"})
@@ -14,11 +18,13 @@ export const OwnershipCheck = async (req: Request, res: Response, next : NextFun
 
         if(project.creatorID == userId){
             next()
+            return
         }
 
         return res.status(403).json({message : "Forbidden: You are not the owner of this project"})
     }
-    catch{
+    catch (error){
+        console.log(error)
         return res.status(500).json({message : "Internal server error"})
     }
 }
